66478e2aa6fcc4e4237407ef8580daeca865be5c
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-05-16 22:48:05 +0900

    기본 UI 세팅

--- CSharp_Inheritance/Form1.Designer.cs
+++ CSharp_Inheritance/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pMain = new System.Windows.Forms.Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btmOneCycle = new System.Windows.Forms.Button();
+            this.btnCycle = new System.Windows.Forms.Button();
+            this.btnCar = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pMain
+            // 
+            this.pMain.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pMain.Location = new System.Drawing.Point(12, 12);
+            this.pMain.Name = "pMain";
+            this.pMain.Size = new System.Drawing.Size(776, 295);
+            this.pMain.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblName.Location = new System.Drawing.Point(12, 328);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(123, 28);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "외발 자전거";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btmOneCycle
+            // 
+            this.btmOneCycle.Location = new System.Drawing.Point(185, 328);
+            this.btmOneCycle.Name = "btmOneCycle";
+            this.btmOneCycle.Size = new System.Drawing.Size(162, 28);
+            this.btmOneCycle.TabIndex = 1;
+            this.btmOneCycle.Text = "1단계 : 외발 자전거";
+            this.btmOneCycle.UseVisualStyleBackColor = true;
+            // 
+            // btnCycle
+            // 
+            this.btnCycle.Location = new System.Drawing.Point(378, 328);
+            this.btnCycle.Name = "btnCycle";
+            this.btnCycle.Size = new System.Drawing.Size(162, 28);
+            this.btnCycle.TabIndex = 2;
+            this.btnCycle.Text = "2단계 : 자전거";
+            this.btnCycle.UseVisualStyleBackColor = true;
+            // 
+            // btnCar
+            // 
+            this.btnCar.Location = new System.Drawing.Point(578, 328);
+            this.btnCar.Name = "btnCar";
+            this.btnCar.Size = new System.Drawing.Size(162, 28);
+            this.btnCar.TabIndex = 3;
+            this.btnCar.Text = "3단계 : 자동차";
+            this.btnCar.UseVisualStyleBackColor = true;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(278, 362);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(162, 51);
+            this.btnLeft.TabIndex = 4;
+            this.btnLeft.Text = "<--------";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(480, 362);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(162, 51);
+            this.btnRight.TabIndex = 5;
+            this.btnRight.Text = "-------->";
+            this.btnRight.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnCar);
+            this.Controls.Add(this.btnCycle);
+            this.Controls.Add(this.btmOneCycle);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pMain);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pMain;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btmOneCycle;
+        private System.Windows.Forms.Button btnCycle;
+        private System.Windows.Forms.Button btnCar;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
     }
 }
 
